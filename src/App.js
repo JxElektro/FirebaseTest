d851ec89e8048d2fe37364be4f9d43d961ecc7c6
Jhen Nuñez <jxelektro@gmail.com>
2022-11-16 20:54:12 -0300

    3 Push Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> 
         <h1>Firebase</h1>
-       
+        <a href="/LoginView">Login</a>
       </header>
     </div>
   );
