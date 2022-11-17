8daefccbcd8f30571c318489835d8e29f24a15bb
Jhen Nuñez <jxelektro@gmail.com>
2022-11-16 22:40:01 -0300

    Fixed google Auth

--- src/routes/loginView.jsx
+++ src/routes/loginView.jsx
@@ -1,37 +1,35 @@
-import {auth} from '../firebase/firebase';
-import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import { auth } from "../firebase/firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 export default function LoginView() {
+  /* This funciton is called when the user clicks the login button and it will create a object of the GoogleAuthProvider
+  and is assigned to the provider variable and then will await the signInWithGoogle function to be called and pass the provider*/
     async function handleOnClick() {
     const googleProvider = new GoogleAuthProvider();
     await signInWithGoogle(googleProvider);
   }
 
+  /* This function will take the provider object and pass it to the signInWithPopup function and then await the result*/
   async function signInWithGoogle(googleProvider) {
     try {
-      const res = await signInWithPopup(auth , googleProvider);
+      const res = await signInWithPopup(auth, googleProvider);
       console.log(res);
     } catch (error) {
       console.log(error);
-
     }
-
   }
-
   return (
     <div className="App">
       <header className="App-header">
-      <h1>LoginView</h1>
+        <h1>LoginView</h1>
+        <button onClick={handleOnClick}>Google</button>
 
-      <button onClick={handleOnClick}>Google</button>
-      
-      <a href="/LoginView">Login</a>
+        <a href="/LoginView">Login</a>
         <a href="/dashboard">Dashboard</a>
         <a href="/dashboard/profile">Edit Profile</a>
         <a href="/signout">Sign Out</a>
         <a href="/u/username">Public Profile</a>
         <a href="/choose-username">Choose Username</a>
-        
       </header>
     </div>
   );
